59c8f3bedfa97d8a5474114fce77de0c7d9a868e
john <lord1229@gmail.com>
2021-12-21 22:43:04 +0900

    kotlin spring project create

---
+++ src/test/kotlin/com/kotlin/exercise/ExerciseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.exercise
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExerciseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
